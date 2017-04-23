915c0fb2a14ef494bf40ee8804038a2e81331eb5
dssaez <dssaez@gmail.com>
2017-04-23 16:33:30 +0100

    Actualización de constants para el desarrollo en development

--- utils/constants.php
+++ utils/constants.php
@@ -1,8 +1,22 @@
 <?php
 
 define('VIEWS', '../views');
-define('DOMAIN_ES', 'www.indanielweb.es');
-define('DOMAIN_EN', 'www.indanielweb.com');
-define('DOMAIN', $_SERVER['HTTP_HOST']);
+/* ##### PRODUCCION ###### */
+/*define('DOMAIN_ES', 'www.sunyogamoon.devel.es');
+define('DOMAIN_EN', 'www.sunyogamoon.devel.com');
+define('DOMAIN', $_SERVER['HTTP_HOST']);*/
+/* ##### PRODUCCION ###### */
 
+/* ##### DEVELOPMENT ###### */
+if ($_SERVER['HTTP_HOST'] == "192.168.1.101"){
+        define('DOMAIN_ES', '192.168.1.101/sunyogamoon.devel.es/');
+        define('DOMAIN_EN', '192.168.1.101/sunyogamoon.devel.com/');
+        define('DOMAIN', $_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"]);
+}
+else{
+        define('DOMAIN_ES', 'www.sunyogamoon.devel.es');
+        define('DOMAIN_EN', 'www.sunyogamoon.devel.com');
+        define('DOMAIN', $_SERVER['HTTP_HOST']);
+}
+/* ##### DEVELOPMENT ###### */
 ?>
